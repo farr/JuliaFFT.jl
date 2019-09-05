abeecbfef161d17b3ce2a8c9e21a525bcf3282c6
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2019-09-04 22:18:42 -0400

    Note that we also need complex exponentiation to make this work.

--- src/JuliaFFT.jl
+++ src/JuliaFFT.jl
@@ -10,9 +10,9 @@ Computes the discrete Fourier transform of the array `xs`.
 ``y_k = \\sum_{j=1}^N \\exp\\left[ - 2 \\pi i \\frac{(j-1)(k-1)}{N} \\right] x_j``
 
 The implementation is fully generic; as long as the input types have
-addition and multiplication (by complex numbers) defined, the algorithm will
-work.  Not particularly fast (about 2ms for FFT of 1024 element array on my
-modern-ish MacBook).
+exponentiation, addition, and multiplication (by complex numbers) defined, the
+algorithm will work.  Not particularly fast (about 2ms for FFT of 1024 element
+array on my modern-ish MacBook).
 
 Currently only works for 1D arrays.
 
