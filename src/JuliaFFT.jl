ba5be0e60fd6c71112c832cc57689c676d278a76
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2019-09-04 22:14:55 -0400

    Move docstring to proper location.

--- src/JuliaFFT.jl
+++ src/JuliaFFT.jl
@@ -2,22 +2,22 @@ module JuliaFFT
 
 export fft
 
+"""
+    fft(xs)
+
+Computes the discrete Fourier transform of the array `xs`.
+
+``y_k = \\sum_{j=1}^N \\exp\\left[ - 2 \\pi i \\frac{(j-1)(k-1)}{N} \\right] x_j``
+
+The implementation is fully generic; as long as the input types have
+addition and multiplication (by complex numbers) defined, the algorithm will
+work.  Not particularly fast (about 2ms for FFT of 1024 element array on my
+modern-ish MacBook).
+
+Currently only works for 1D arrays.
+
+"""
 function fft(xs)
-    """
-        fft(xs)
-
-    Computes the discrete Fourier transform of the array `xs`.
-
-    ``y_k = \\sum_{j=1}^N \\exp\\left[ - 2 \\pi i \\frac{(j-1)(k-1)}{N} \\right] x_j``
-
-    The implementation is fully generic; as long as the input types have
-    addition and multiplication (by complex numbers) defined, the algorithm will
-    work.  Not particularly fast (about 2ms for FFT of 1024 element array on my
-    modern-ish MacBook).
-
-    Currently only works for 1D arrays.
-
-    """
     n = size(xs,1)
 
     if n == 1
